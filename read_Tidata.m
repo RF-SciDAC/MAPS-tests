418034105fbccdc66755b3b7cc057e7d1eaad0f9
rhealbarnett <rhealbarnett@gmail.com>
2020-10-08 15:48:32 +1100

    Updated figure axes labels and title.

--- read_Tidata.m
+++ read_Tidata.m
@@ -3,7 +3,7 @@ num_order = 5;
 
 Ti_arr = zeros(num_refine,num_order);
 
-file_path = '/Volumes/DATA/postdoc/codes/decreased-newtoniter/';
+file_path = '/Volumes/DATA/postdoc/mfem/benchmarking/srtol_1e-12/';
 
 for ii=1:num_refine
     
@@ -25,16 +25,25 @@ xmax = 1.0;
 ymin = 0.0;
 ymax = 1.0;
 
-npts = 4; 
+npts_arr = zeros(1,length(refine));
+npts_arr(1,1) = 4; 
 
-dx = (xmax - xmin)./(npts.*refine);
+for ii=2:length(refine)
+    npts_arr(1,ii) = 2*npts_arr(1,ii-1);
+end
+
+dx = (xmax - xmin)./(npts_arr);
 
 figure(1)
+set(gcf,'Color','white')
 loglog(dx,err_arr(:,1),'o-')
 hold on
 loglog(dx,err_arr(:,2),'*-')
 loglog(dx,err_arr(:,3),'v-')
 loglog(dx,err_arr(:,4),'d-')
 loglog(dx,err_arr(:,5),'+-')
-xlim([5.0e-2 0.3])
+% xlim([5.0e-2 0.3])
+title('$\chi_{\parallel} = 10^3$','interpreter','latex')
+xlabel('$dx$','interpreter','latex')
+ylabel('Error ($|T^{-1}(0,0) - 1|$)', 'interpreter','latex')
 legend('O1','O2','O3','O4','O5')
