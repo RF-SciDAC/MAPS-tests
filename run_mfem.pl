9ae9623d6d444c170d69fd6069ba6ae42de0f56d
rhealbarnett <rhealbarnett@gmail.com>
2020-11-30 10:09:00 +1100

    Included new inputs in call to transport2d.cpp.

--- run_mfem.pl
+++ run_mfem.pl
@@ -15,7 +15,7 @@ for ($r = 1; $r < 2; $r = $r + 1) {
 		
 		open (my $file, '>', 'output.txt') or die "Could not open file: $!";
 	
-		$command = $transport." -rs ".$r." -o ".$o." -m ".$mesh_path."inline-quad.mesh"." -bc ".$root_dir."transport2d_bcs.inp"." -ic ".$root_dir."transport2d_ics.inp"." -ec ".$root_dir."transport2d_ecs.inp"." -op 8 -l 1 -visit -dt 1.0e-2 -tf 100 -eqn-w '1 1 1 1 1' -vs 1 -p 0 -es ".$root_dir."transport2d_ess.inp"." -srtol 1.0e-8 -satol 1.0e-8 -natol 1.0e-7 -nrtol 1.0e-7";	
+		$command = "mpirun -np 2 "$transport." -rs ".$r." -o ".$o." -m ".$mesh_path."inline-quad.mesh"." -bc ".$root_dir."transport2d_bcs.inp"." -ic ".$root_dir."transport2d_ics.inp"." -ec ".$root_dir."transport2d_ecs.inp"." -op 8 -l 1 -visit -dt 1.0e-2 -tf 100 -eqn-w '1 1 1 1 1' -vs 1 -p 0 -es ".$root_dir."transport2d_ess.inp"." -srtol 1.0e-8 -satol 1.0e-8 -natol 1.0e-10 -nrtol 1.0e-10 -latol 1.0e-6 -lrtol 1.0e-6 -lmaxit 500";	
 
         	$output = `$command`;
 		
