a2f15c1c254d9c572fd005b126c2f58abf500510
gz6 <gz6@ornl.gov>
2020-11-29 18:18:39 -0500

    Edited file paths for fusiont6.

--- run_mfem.pl
+++ run_mfem.pl
@@ -5,15 +5,15 @@ use warnings;
 $r = 1;
 $o = 1;
 
-$transport = '~/Documents/MFEM/mfem/miniapps/plasma/transport2d';
-$root_dir = '~/Documents/MFEM/mfem/miniapps/plasma/';
+$transport = '../mfem/miniapps/plasma/';
+$root_dir = '..//mfem/miniapps/plasma/';
 $prefix = 'Transport2D-Parallel';
-$mesh_path = '~/Documents/MFEM/mfem/data/';
+$mesh_path = '../mfem/data/';
 
 for ($r = 1; $r < 2; $r = $r + 1) {
 	for ($o = 1; $o < 2; $o = $o + 1) {
 		
-		open (my $file, '>', 'output.txt') or die "Could not open file: $!";
+		open (my $file, '>', $r"_"$o"_output.txt") or die "Could not open file: $!";
 	
 		$command = "mpirun -np 2 "$transport." -rs ".$r." -o ".$o." -m ".$mesh_path."inline-quad.mesh"." -bc ".$root_dir."transport2d_bcs.inp"." -ic ".$root_dir."transport2d_ics.inp"." -ec ".$root_dir."transport2d_ecs.inp"." -op 8 -l 1 -visit -dt 1.0e-2 -tf 100 -eqn-w '1 1 1 1 1' -vs 1 -p 0 -es ".$root_dir."transport2d_ess.inp"." -srtol 1.0e-8 -satol 1.0e-8 -natol 1.0e-10 -nrtol 1.0e-10 -latol 1.0e-6 -lrtol 1.0e-6 -lmaxit 500";	
 
