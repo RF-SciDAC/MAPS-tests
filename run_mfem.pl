7f109032964eb5404c100afb61ba40382e9904d3
gz6 <gz6@ornl.gov>
2020-11-30 03:02:03 -0500

    Commented out die if command.

--- run_mfem.pl
+++ run_mfem.pl
@@ -5,22 +5,28 @@ use warnings;
 $r = 1;
 $o = 1;
 
-$transport = '../mfem/miniapps/plasma/transport2d';
-$root_dir = '..//mfem/miniapps/plasma/';
+$transport = '../../mfem/miniapps/plasma/transport2d';
+$root_dir = '../../mfem/miniapps/plasma/';
 #$prefix = 'Transport2D-Parallel';
-$mesh_path = '../mfem/data/';
+$input_path = '~/mfem-analysis/input-files/';
+$mesh_path = '../../mfem/data/';
 
-for ($r = 1; $r < 2; $r = $r + 1) {
-	for ($o = 1; $o < 2; $o = $o + 1) {
-		
-		open (my $file, ">", "".$r."_".$o."_output.out") or die "Could not open file: $!";
+for ($r = 1; $r < 6; $r = $r + 1) {
+	for ($o = 1; $o < 6; $o = $o + 1) {
 	
-		$command = "mpirun -np 16 ".$transport." -rs ".$r." -o ".$o." -m ".$mesh_path."inline-quad.mesh"." -bc ".$root_dir."transport2d_bcs.inp"." -ic ".$root_dir."transport2d_ics.inp"." -ec ".$root_dir."transport2d_ecs.inp"." -op 8 -l 1 -visit -dt 1.0e-2 -tf 100 -eqn-w '1 1 1 1 1' -vs 1 -p 0 -es ".$root_dir."transport2d_ess.inp"." -srtol 1.0e-8 -satol 1.0e-8 -natol 1.0e-10 -nrtol 1.0e-10 -latol 1.0e-6 -lrtol 1.0e-6 -lmaxit 500";	
+		my $folder = "r".$r."_o".$o."";
+		mkdir($folder, 0755);
+		chdir($folder) or die "can't chdir $folder\n";	
+		open (my $file, ">", "output.out") or die "Could not open file: $!";
+	
+		$command = "mpirun -np 16 ".$transport." -rs ".$r." -o ".$o." -m ".$mesh_path."inline-quad.mesh"." -bc ".$root_dir."transport2d_bcs.inp"." -ic ".$root_dir."transport2d_ics.inp"." -ec ".$input_path."transport2d_ecs.inp"." -op 8 -l 1 -visit -dt 1.0e-2 -tf 100 -eqn-w '1 1 1 1 1' -vs 4 -p 0 -es ".$root_dir."transport2d_ess.inp"." -srtol 1.0e-8 -satol 1.0e-8 -natol 1.0e-10 -nrtol 1.0e-10 -latol 1.0e-6 -lrtol 1.0e-6 -lmaxit 500 -llog 1";	
 
 		$output = `$command`;
 		
-		die "$!" if $?;
-		print $file $output;	
+		#die "$!" if $?;
+		print $file $output;
+
+		chdir("../");
 		
 	}
 }
