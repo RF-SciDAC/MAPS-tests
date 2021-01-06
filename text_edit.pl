b92e2b6e3be4f610251f09b309b0d1a0d992f41b
gz6 <gz6@ornl.gov>
2021-01-05 19:15:19 -0500

    Changed refinement and order to match Sovinec cases.

--- text_edit.pl
+++ text_edit.pl
@@ -2,7 +2,13 @@
 use warnings;
 use strict;
 
-my @chi = qw(1.0e3 1.0e4 1.0e5 1.0e6 1.0e7 1.0e8 1.0e9);
+# Need to edit file/dir paths to wherever these are living.
+my $transport = '../../mfem/miniapps/plasma/transport2d';
+my $root_dir = '../../mfem/miniapps/plasma/';
+my $input_path = '~/mfem-analysis/input-files/';
+my $mesh_path = '../../mfem/data/';
+
+my @chi = qw(1.0e3, 1.0e4, 1.0e5, 1.0e6, 1.0e7, 1.0e8, 1.0e9);
 foreach my $chi (@chi) {
 	my $filename = 'input-files/transport2d_ecs.inp';
 	my $newfile = "$filename.new";
@@ -20,4 +26,19 @@ foreach my $chi (@chi) {
 	close $out or die $!;
 	rename $filename, $backup or die $!;
 	rename $newfile, $filename or die $!;
+
+	my $folder = "chi".$chi;
+        mkdir($folder, 0755);
+        chdir($folder) or die "can't chdir $folder\n";
+        open (my $file, ">", "output.out") or die "Could not open file: $!";
+
+        my $command = "mpirun -np 16 ".$transport." -rs 1 -o 3 -m ".$mesh_path."inline-quad.mesh"." -bc ".$root_dir."transport2d_bcs.inp"." -ic ".$root_dir."transport2d_ics.inp"." -ec ".$input_path."transport2d_ecs.inp"." -op 8 -l 1 -visit -dt 1.0e-2 -tf 100 -eqn-w '1 1 1 1 1' -vs 4 -p 0 -es ".$root_dir."transport2d_ess.inp"." -srtol 1.0e-8 -satol 1.0e-8 -natol 1.0e-14 -nrtol 1.0e-10 -latol 1.0e-10 -lrtol 1.0e-14 -pt 1";
+ 
+        my $output = `$command`;
+ 
+        die "$!" if $?;
+        print $file $output;
+
+        chdir("../");
+
 }
