fd2d61fb8f99463ced5b7e54d62abf810227c98b
gz6 <gz6@ornl.gov>
2021-04-14 23:59:37 -0400

    Updating all changes

--- run_mfem.pl
+++ run_mfem.pl
@@ -11,12 +11,12 @@ $root_dir = '../../mfem/miniapps/plasma/';
 $input_path = '../input-files/';
 $mesh_path = '../../mfem/data/';
 
-for ($o = 3; $o < 6; $o = $o + 1) {
-	for ($r = 0; $r < 5; $r = $r + 1) {
+for ($o = 3; $o < 4; $o = $o + 1) {
+	for ($r = 1; $r < 2; $r = $r + 1) {
 		
 		#next if($o == 4 && ($r == 4 || $r == 5));
-		next if($o == 5 && ($r == 2 || $r == 3));
-		next if($o == 4 && ($r == 3));
+		#next if($o == 5 && ($r == 2 || $r == 3));
+		#next if($o == 4 && ($r == 3));
 		#next if($o == 1 && ($r == 0 || $r == 1 || $r == 2));
 
 		my $folder = "r".$r."_o".$o."";
@@ -24,7 +24,7 @@ for ($o = 3; $o < 6; $o = $o + 1) {
 		chdir($folder) or die "can't chdir $folder\n";	
 		open (my $file, ">", "output.out") or die "Could not open file: $!";
 	
-		$command = "mpirun -np 1 ".$transport." -rs ".$r." -o ".$o." -m ".$mesh_path."inline-quad.mesh"." -bc ".$input_path."transport2d_bcs.inp"." -ic ".$input_path."transport2d_ics.inp"." -ec ".$input_path."transport2d_ecs.inp"." -op 8 -l 1 -visit -dt 1.0e-2 -tf 200 -eqn-w '1 1 1 1 1' -vs 4 -p 0 -es ".$input_path."transport2d_ess.inp"." -srtol 1.0e-8 -satol 1.0e-8 -natol 1.0e-10 -nrtol 1.0e-10 -latol 1.0e-6 -lrtol 1.0e-6 -pt 2 -no-amr -dgk 100 -nmaxit 1";	
+		$command = "mpirun -np 16 ".$transport." -rs ".$r." -o ".$o." -m ".$mesh_path."inline-quad.mesh"." -bc ".$input_path."transport2d_bcs.inp"." -ic ".$input_path."transport2d_ics.inp"." -ec ".$input_path."transport2d_ecs.inp"." -op 8 -l 1 -visit -dt 1.0e-2 -tf 200 -eqn-w '1 1 1 1 1' -vs 4 -p 0 -es ".$input_path."transport2d_ess.inp"." -srtol 1.0e-8 -satol 1.0e-8 -natol 1.0e-10 -nrtol 1.0e-10 -latol 1.0e-6 -lrtol 1.0e-6 -pt 1 -nmaxit 1 -no-amr -lmaxit 300";	
 
 		$output = `$command`;
 
