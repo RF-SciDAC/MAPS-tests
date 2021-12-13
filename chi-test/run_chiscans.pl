5e8a83d6ca1bd3bbe8486523363a545952a11af5
gz6 <gz6@ornl.gov>
2021-12-13 10:57:32 -0500

    Moving momentum MMS test data.

--- chi-test/run_chiscans.pl
+++ chi-test/run_chiscans.pl
@@ -8,8 +8,8 @@ my $root_dir = '~/mfem/miniapps/plasma/';
 my $input_path = '../input-files/';
 my $mesh_path = '~/mfem/data/';
 
-#my @chi = qw(1.0e3 1.0e4 1.0e5 1.0e6 1.0e7);
-my @chi = qw(1.0e6);
+my @chi = qw(1.0e3 1.0e4);
+#my @chi = qw(1.0e6);
 foreach my $chi (@chi) {
 	my $filename = 'input-files/transport2d_ecs.inp';
 	my $newfile = "$filename.new";
@@ -19,9 +19,9 @@ foreach my $chi (@chi) {
 	open my $out, '>', $newfile or die $!;
 
 	while(<$in>) {
-		s/.*/ConstantCoefficient ${chi} / if $.==22;
+		s/.*/ConstantCoefficient ${chi} / if $.==20;
 		print {$out} $_;
-		print if ($.==21 || $.==22);
+		print if ($.==19 || $.==20);
 	}
 
 	close $out or die $!;
@@ -33,7 +33,7 @@ foreach my $chi (@chi) {
         chdir($folder) or die "can't chdir $folder\n";
         open (my $file, ">", "output.out") or die "Could not open file: $!";
 
-        my $command = "mpirun -np 16 ".$transport." -rs 1 -o 3 -m ".$mesh_path."inline-quad.mesh"." -bc ".$input_path."transport2d_bcs.inp"." -ic ".$input_path."transport2d_ics.inp"." -ec ".$input_path."transport2d_ecs.inp"." -op 2 -l 1 -visit -dt 1.0e-2 -tf 100 -eqn-w '1 1 1 1 1' -term-flags '-1 1 -1 -1 -1' -vs 4 -p 0 -es ".$input_path."transport2d_ess.inp"." -srtol 1.0e-8 -satol 1.0e-8 -natol 1.0e-10 -nrtol 1.0e-10 -latol 1.0e-6 -lrtol 1.0e-6 -lmaxit 500 -pt 1 -no-amr -nmaxit 10";
+        my $command = "mpirun -np 16 ".$transport." -rs 1 -o 3 -m ".$mesh_path."inline-quad.mesh"." -bc ".$input_path."transport2d_bcs.inp"." -ic ".$input_path."transport2d_ics.inp"." -ec ".$input_path."transport2d_ecs.inp"." -op 2 -l 1 -visit -dt 1.0e-2 -tf 100 -eqn-w '1 1 1 1 1' -term-flags '-1 17 -1 -1 -1' -vs 4 -p 0 -es ".$input_path."transport2d_ess.inp"." -srtol 1.0e-8 -satol 1.0e-8 -natol 1.0e-10 -nrtol 1.0e-10 -latol 1.0e-6 -lrtol 1.0e-6 -lmaxit 500 -pt 1 -no-amr -nmaxit 10";
  
 	my $output = `$command`;
  	
