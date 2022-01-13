afafa1d1ba11f9d936730319cae7ee1924686467
David Green <david.lindsay.green@gmail.com>
2022-01-13 12:47:23 -0500

    fixed matlab script

--- run_mfem.m
+++ run_mfem.m
@@ -1,6 +1,7 @@
-get_values = '~/mfem/miniapps/tools/get-values';
-transport = '~/mfem/miniapps/plasma/transport2d';
-input_dir = '~/mfem-analysis/mom_test/';
+root = '/home/gz6';
+get_values = [root,'/mfem/miniapps/tools/get-values'];
+transport = [root,'/mfem/miniapps/plasma/transport2d'];
+input_dir = [root,'/mfem-analysis/testing/mom_test/'];
 prefix = 'Transport2D-Parallel';
 
 [status,git_hash] = system('git rev-parse HEAD');
@@ -11,7 +12,7 @@ header = [s1 s2];
 anisoTest = 0;
 mmsTest = 1;
 
-npArray = [32,64,128,256,512];
+npArray = ["8","16","32","64","128","256","512"];
 
 if anisoTest
 
@@ -29,35 +30,61 @@ if anisoTest
 
 elseif mmsTest
 
+% Save library paths
+MatlabPath = getenv('LD_LIBRARY_PATH');
+% Make Matlab use system libraries
+setenv('LD_LIBRARY_PATH',getenv('PATH'));
+
+order_vals = [2,3,4];
+
+for order=order_vals
     for ii=1:length(npArray)
 
-        dirName = strcat(input_dir,"run-test/O3_N",num2str(npArray(ii)));
+        old_dir = pwd();
+        dirName = strcat("run-test/O3_N",npArray(ii));
         mkdir([dirName])
         cd([dirName])
 
-        order = 3;
+        %order = 3;
+        %dt = 1e-12;
 
-        fprintf('Running transport2d for NP %d and order %d\n',npArray(ii),order)
+        fprintf('Running transport2d for NP %s and order %d\n',npArray(ii),order)
 
-        mesh_path = '~/mfem-analysis/mom_test/slab_';
+        mesh_path = [root,'/mfem-analysis/testing/mom_test/slab_',char(npArray(ii)),'.mesh'];
+        %mesh_path = [root,'/mfem/data/'];
+
+        %/home/gz6/mfem-analysis/testing/mom_test/slab_64.mesh
 
         command = strcat(transport," -o ",num2str(order),...
-        " -m ",strcat(mesh_path,num2str(npArray(ii)),'.mesh')," -bc ",strcat(input_dir,"/transport2d_bcs.inp"),...
+        " -m ",string(mesh_path)," -bc ",strcat(input_dir,"/transport2d_bcs.inp"),...
         " -ic ",strcat(input_dir,"/transport2d_ics.inp"), " -ec ",strcat(input_dir,"/transport2d_ecs.inp"),...
-        " -op 4 -l 1 -visit -dt 1.0e-12 -tf 1.0e-12 -eqn-w '1 1 1 1 1' -vs 1 -p 0 -es ",...
+        " -op 4 -l 1 -visit -dt 1.0e-16 -tf 1.0e-16 -eqn-w '1 1 1 1 1' -vs 1 -p 0 -es ",...
         strcat(input_dir,"/transport2d_ess.inp"),...
         " -term-flags '-1 -1 11 -1 -1' -natol 1e-12 -nrtol 1e-12 -latol 1e-12 ",...
-        "-lrtol 1e-12 -dza 0 -dzb 1e20 -fld-m '1 1 1 1 1' -no-amr")
-
-        [status,output] = system(command)
-
-        writematrix(output,'output.dat','delimiter','tab');
-        writematrix(status,'status.dat','delimiter','tab');
-        
-        cd('../')
-
+        string(['-lrtol 1e-12 -dza 0 -dzb 1e20 -fld-m "1 1 1 1 1" -no-amr ','-rs ',num2str(-1),' > output.out']));
+            
+       system(command);
+       t = table2array(readtable('transport2d_err.out','FileType','text'));
+       error(order,ii) = t(2,7);
+       cd(old_dir);
+%     [status,output] = system(command);
 
     end
+end
+% Reassign old library paths
+setenv('LD_LIBRARY_PATH',MatlabPath)
 
-
+for i=1:numel(npArray)
+    num_points(i) = str2num(npArray(i));
+end
+figure()
+for order=order_vals
+loglog(num_points,error(order,:),'Marker','o','DisplayName',['Order=',num2str(order)])
+hold on
+end
+hold off
+legend
+title('Error(resolution)');
+ylabel('Error');
+xlabel('num grid points');
 end
